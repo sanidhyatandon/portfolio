c67be61beebea71733777344300afff4fc3d0668
sanidhyatandon <sanidhya.tandon@gmail.com>
2015-11-12 02:55:23 +0530

    trial1

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151104183626) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.string   "image"
