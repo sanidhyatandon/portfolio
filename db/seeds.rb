e879441abaa63270bc0c74bf938c419272ab6252
sanidhyatandon <sanidhya.tandon@gmail.com>
2015-11-12 02:07:07 +0530

    testing left

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,12 @@
 
 
 t1 = Tag.create(title: "Projects", image: "computer.jpg")
-Project.create(name: "Travel App", image:"tour-guide.jpg", description: "The beach of Ipanema is known for its elegant development and its social life.",  tag_id: t1.id)
-Project.create(name: "Threadly", image:"threadly.jpg", description: "The western coastline contains the island's finest beaches.", tag_id: t1.id)
-Project.create(name: "Innovation Cloud", image:"innovation-cloud.jpg" , description: "An elite destination famous for its white sand beaches", tag_id: t1.id)
+Project.create(name: "Travel App", image:"tour-guide.jpg", description:"It is a tour guide built using ruby on rails framework.It has different categories of destinations and the user can view description of a particular destination. The user can edit the title and description of the destination.",  tag_id: t1.id)
+Project.create(name: "Threadly", image:"threadly.jpg", description: "Web-app built using Ruby on Rails that demonstrates database connectivity.The user can enter their thaughts and then view them later anytime.", tag_id: t1.id)
+Project.create(name: "Innovation Cloud", image:"innovation-cloud.jpg" , description: "Web-app built using Ruby on Rails that demonstrates database connectivity.", tag_id: t1.id)
 
 t2 = Tag.create(title: "Internships", image: "internships-abroad-programs.jpg")
-Project.create(name: "Tata Consultancy Services",image:"tcs.jpg", description: "Machu Picchu was built around 1450, at the height of the Inca Empire.", tag_id: t2.id)
-Project.create(name: "Xeeva, Inc.",image:"xeeva.jpg", description: "The meeting place of the two houses of the Parliament of the United Kingdom.", tag_id: t2.id)
+Project.create(name: "Tata Consultancy Services",image:"tcs.jpg", description: "I worked under the supervision of Senior Storage Architect.I designed the front-end of Conformance Assurance Solution(CAS) using HTML-5, CSS-3, Bootsrap-3, Javascipt and J-Query. CAS is an internal project of TCS that ensures that the cloud vendor products conform with the CDMI(Cloud Data Management Interface) specification of SNIA(Storage Networking Industry Association).
+	", tag_id: t2.id)
+Project.create(name: "Xeeva, Inc.",image:"xeeva.jpg", description: "I worked under the supervision of Senior Testing Manager.I studied the buisness requirement specification documents thorougly and performed manual testing by creating test cases and test scenarios.", tag_id: t2.id)
 
