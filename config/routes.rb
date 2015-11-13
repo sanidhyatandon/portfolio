9a881e2ac688e9663fc1fb741cd9779a4053da08
sanidhyatandon <sanidhya.tandon@gmail.com>
2015-11-14 02:38:41 +0530

    trial updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/projects/:id' => 'projects#show', as: :project
   get '/about' => 'pages#about', as: :about
   get '/contact' => 'pages#contact', as: :contact
-  
+ 
 
 
 
